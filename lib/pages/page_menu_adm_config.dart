bdacf5e80774bf616a999b6664e79b667bd579cc
unknown <masyordan@gmail.com>
2024-11-02 16:19:22 +0800

    tambah icon search di halaman config

--- lib/pages/page_menu_adm_config.dart
+++ lib/pages/page_menu_adm_config.dart
@@ -1,3 +1,4 @@
+/*
 //kode ke-2
 import 'dart:convert'; // untuk jsonDecode
 import 'package:flutter/material.dart';
@@ -39,10 +40,12 @@ class _PageConfigState extends State<PageConfig> {
   }
 
   // Fungsi untuk mengirim data update ke server
-  Future<void> _updateData(int index, String newName, String newNrp) async {
+  Future<void> _updateData(
+      int index, String newName, String newNrp, String newCrew) async {
     String apiUrl = "http://209.182.237.240:1880/edit";
 
-    var body = jsonEncode({"index": index, "nama": newName, "nrp": newNrp});
+    var body = jsonEncode(
+        {"index": index, "nama": newName, "nrp": newNrp, "crew": newCrew});
 
     final response = await http.post(
       Uri.parse(apiUrl),
@@ -64,11 +67,14 @@ class _PageConfigState extends State<PageConfig> {
   }
 
   // Fungsi untuk menampilkan dialog pop-up edit
-  void _editItem(int index, String currentName, String currentNrp) {
+  void _editItem(
+      int index, String currentName, String currentNrp, String currentCrew) {
     TextEditingController nameController =
         TextEditingController(text: currentName);
     TextEditingController nrpController =
         TextEditingController(text: currentNrp);
+    TextEditingController crewController =
+        TextEditingController(text: currentCrew);
 
     showDialog(
       context: context,
@@ -87,11 +93,7 @@ class _PageConfigState extends State<PageConfig> {
                 decoration: const InputDecoration(labelText: 'NRP'),
               ),
               TextField(
-                controller: nameController,
-                decoration: const InputDecoration(labelText: 'Section'),
-              ),
-              TextField(
-                controller: nrpController,
+                controller: crewController,
                 decoration: const InputDecoration(labelText: 'Crew'),
               ),
             ],
@@ -106,8 +108,11 @@ class _PageConfigState extends State<PageConfig> {
             ElevatedButton(
               child: const Text('Update'),
               onPressed: () async {
-                await _updateData(index, nameController.text,
-                    nrpController.text); // Kirim data ke server
+                await _updateData(
+                    index,
+                    nameController.text,
+                    nrpController.text,
+                    crewController.text); // Kirim data ke server
                 Navigator.of(context).pop(); // Tutup dialog setelah update
               },
             ),
@@ -145,15 +150,15 @@ class _PageConfigState extends State<PageConfig> {
               return ListTile(
                 leading: CircleAvatar(child: Text(item['no'].toString())),
                 title: Text(item['nama']),
-                subtitle: Text('(${item['nrp']})'),
+                subtitle: Text('(${item['nrp']})\n${item['crew']}'),
                 trailing: Row(
                   mainAxisSize: MainAxisSize.min,
                   children: [
                     IconButton(
                       icon: const Icon(Icons.edit, color: Colors.blue),
                       onPressed: () {
-                        _editItem(index, item['nama'],
-                            item['nrp']); // Panggil fungsi edit
+                        _editItem(index, item['nama'], item['nrp'],
+                            item['crew']); // Panggil fungsi edit
                       },
                     ),
                     IconButton(
@@ -169,6 +174,222 @@ class _PageConfigState extends State<PageConfig> {
           );
         },
       ),
+    );
+  }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    home: PageConfig(),
+  ));
+}
+*/
+
+//kode ke-3
+import 'dart:convert'; // untuk jsonDecode
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../config/config.dart';
+import 'package:new_qi_apps/auth/auth_service.dart';
+
+class PageConfig extends StatefulWidget {
+  const PageConfig({super.key});
+
+  @override
+  _PageConfigState createState() => _PageConfigState();
+}
+
+class _PageConfigState extends State<PageConfig> {
+  List<dynamic> _data = []; // Data yang diambil dari API
+  List<dynamic> _filteredData = []; // Data yang difilter berdasarkan pencarian
+  TextEditingController searchController = TextEditingController();
+  bool _isSearching = false;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData(); // Mengambil data ketika halaman dimuat
+  }
+
+  String _buildApiUrl() {
+    return "http://$apiIP:$apiPort/api/mp";
+  }
+
+  // Fungsi untuk mengambil data dari API
+  Future<void> fetchData() async {
+    String apiUrl = _buildApiUrl();
+    final response = await http.get(
+      Uri.parse(apiUrl),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $userToken', // Menyertakan token ke header
+      },
+    );
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      setState(() {
+        _data = data['response'];
+        _filteredData = _data;
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  // Fungsi untuk mengirim data update ke server
+  Future<void> _updateData(
+      int index, String newName, String newNrp, String newCrew) async {
+    String apiUrl = "http://209.182.237.240:1880/edit";
+
+    var body = jsonEncode(
+        {"index": index, "nama": newName, "nrp": newNrp, "crew": newCrew});
+
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $userToken', // Sertakan token
+      },
+      body: body,
+    );
+
+    if (response.statusCode == 200) {
+      print("Update successful");
+      fetchData(); // Refresh data setelah update
+    } else {
+      throw Exception('Failed to update data');
+    }
+  }
+
+  // Fungsi untuk menampilkan dialog pop-up edit
+  void _editItem(
+      int index, String currentName, String currentNrp, String currentCrew) {
+    TextEditingController nameController =
+        TextEditingController(text: currentName);
+    TextEditingController nrpController =
+        TextEditingController(text: currentNrp);
+    TextEditingController crewController =
+        TextEditingController(text: currentCrew);
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Edit Data'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(labelText: 'Nama'),
+              ),
+              TextField(
+                controller: nrpController,
+                decoration: const InputDecoration(labelText: 'NRP'),
+              ),
+              TextField(
+                controller: crewController,
+                decoration: const InputDecoration(labelText: 'Crew'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Tutup dialog
+              },
+            ),
+            ElevatedButton(
+              child: const Text('Update'),
+              onPressed: () async {
+                await _updateData(
+                    index,
+                    nameController.text,
+                    nrpController.text,
+                    crewController.text); // Kirim data ke server
+                Navigator.of(context).pop(); // Tutup dialog setelah update
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Fungsi untuk memfilter data berdasarkan NRP
+  void _filterData(String query) {
+    setState(() {
+      _filteredData = _data
+          .where((item) => item['nrp']
+              .toString()
+              .toLowerCase()
+              .contains(query.toLowerCase()))
+          .toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: _isSearching
+            ? TextField(
+                controller: searchController,
+                autofocus: true,
+                decoration: const InputDecoration(
+                  hintText: 'Cari berdasarkan NRP',
+                ),
+                onChanged: (query) => _filterData(query),
+              )
+            : const Text('Data Man Power'),
+        actions: [
+          IconButton(
+            icon: Icon(_isSearching ? Icons.close : Icons.search),
+            onPressed: () {
+              setState(() {
+                _isSearching = !_isSearching;
+                if (!_isSearching) {
+                  searchController.clear();
+                  _filteredData = _data;
+                }
+              });
+            },
+          ),
+        ],
+      ),
+      body: _filteredData.isEmpty
+          ? const Center(child: Text('No Data Available'))
+          : ListView.builder(
+              itemCount: _filteredData.length,
+              itemBuilder: (context, index) {
+                var item = _filteredData[index];
+                return ListTile(
+                  leading: CircleAvatar(child: Text(item['no'].toString())),
+                  title: Text(item['nama']),
+                  subtitle: Text('(${item['nrp']})\n${item['crew']}'),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.edit, color: Colors.blue),
+                        onPressed: () {
+                          _editItem(index, item['nama'], item['nrp'],
+                              item['crew']); // Panggil fungsi edit
+                        },
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete, color: Colors.red),
+                        onPressed: () {
+                          // Fungsi hapus bisa ditambahkan di sini
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
     );
   }
 }
